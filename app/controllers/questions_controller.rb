e079756f2398b60daca732005d9ce5be91c6104f
Ben Hartney <benhartney@users.noreply.github.com>
2022-04-16 21:04:30 +1000

    Update questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,8 +2,12 @@ class QuestionsController < ApplicationController
     def index
         @questions = Question.all
         @response = @questions.map do |question|
-            {id: question.external_id, data: question.data_from_api}
+            {id: question.external_id, data: JSON.parse(question.data_from_api)}
         end
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
         render :json => @response 
     end
 end
